1c67f17e6b9bd9d39db5991dfb79515b07960d73
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-08-06 22:25:10 +0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,6 @@
+"""my first python file"""
+
+
 # str,int,float,complex,dict,list,tuple,range,set,frozenset,bool,bytes,bytearray,memoryview
 
 # def main():
@@ -7,9 +10,23 @@
 
 # print(main) # <function main at 0x000001F5588804A0>
 
-x = 10
-x = 5
-print(x + 10) # 15
+# x = 10
+# x = 5
+# print(x + 10) # 15
 
 
+u = "users/images/user1.png" # notogri !
+user_profile_image_url = "users/images/user1.png" # togri !
 
+# Javascript 
+# Camel Case
+userProfileImageUrl = "users/images/user1.png"
+# Python 
+# Kebab Case
+user_profile_image_url = "users/images/user1.png" # togri !
+
+import keyword
+print(keyword.kwlist)
+
+# Comment
+# Ctrl + / > Qatorni komentga oladi 

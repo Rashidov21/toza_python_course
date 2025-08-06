69a90ae6abf8af52d3981fb3e94ace3df5e08408
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-08-06 21:26:14 +0500

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,15 @@
+# str,int,float,complex,dict,list,tuple,range,set,frozenset,bool,bytes,bytearray,memoryview
+
+# def main():
+#     print("hello world")
+    
+
+
+# print(main) # <function main at 0x000001F5588804A0>
+
+x = 10
+x = 5
+print(x + 10) # 15
+
+
+

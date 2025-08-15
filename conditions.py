8f6108752ba72a683033d356aa716d5b444681d0
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-08-15 21:55:39 +0500

    Create conditions.py

---
+++ conditions.py
@@ -0,0 +1,53 @@
+# if / elif / else  - agar / aks holda yoki / aks holda
+# input() - userdan string qabul qilish
+# age = int(input("Son kiriting : "))
+
+# if age >= 18:
+#    print("welcome!")
+# elif age == 13:
+#     print("nice!")
+# elif age == 15:
+#     print("nice! 15!")
+# elif age == 16:
+#     print("nice! 16 !")
+# else:
+#     print("error!")   
+     
+# x = 3
+# if x > 5:print("katta")
+
+# ternary if else 
+# temp = 12
+# weather = "issiq" if temp > 20 else "o'rta" 
+# print(weather)
+
+# task  1
+# userdan son qabul qiling agar son toq bo'lsa uni 2 ga ko'paytiring agar juft bolsa 2 ayirish kerak 
+# input : 3
+# output: 6
+# input : 4
+# output: 2
+
+# cash = 350
+# sold = ("Sotilmadi","Sotildi")[cash >= 300]
+# print(sold)
+
+
+# nested 
+# num = -12
+# if num % 2 == 0:
+#     x = 0
+#     r = 1
+#     result = x + r
+#     if num > 0:
+#         print("Correct !")
+#     else:
+#         print("Incorrect !")
+# else:
+#     print("Try again !")
+
+a = int(input("A = ?"))
+if a > 0:
+    print("True")
+else:
+    print("False")

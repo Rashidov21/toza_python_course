546c97b1dae611afdcd21c24996d20062cd1e434
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-08-18 21:45:05 +0500

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,65 @@
+# for , while 
+
+# for - sanoqli takrorlanish  
+# while - cheksiz takrorlanish 
+
+# i > iteration > takrorlanish
+
+# for i in "salom":
+#     print(i)
+
+# for i in range(1,11):
+#     print(f"{i} + 3 = {i+3}")
+    
+# for <sanoq boshi> in <ketma-ketlik>:
+#     har bir siklda qilinishi kerak bolgan ishlar 
+
+# for i in range(3):
+#     n = int(input())
+#     print(n ** 2)
+
+# for i in range(5):print(i)
+
+# l = ["salom",True,1,2]
+# for i in l:
+#     print(i)
+
+# alpha = list("abcdefg")
+# for idx,elem in enumerate(alpha):
+#     print(idx)
+#     print(elem)
+
+# for i in range(10):
+#     print(i)
+# else:
+#     print("Sikl tugadi")
+
+# count = 0
+
+# while True:
+#     count += 1
+#     if count % 10 == 0:
+#         continue # siklni shu joyida toxtatib keyingi siklga o'tish
+#     if count == 1001:
+#         break # siklni majburiy to'xtatish
+#     print(count)
+
+# i = 0
+# while i < 10:
+#     i += 1
+#     print(i) 
+
+# while False:
+#     print("OK")
+
+# summa = 0
+# while True:
+#     user = input("Son kirit/ hisoblash uchun 'stop' deb yozing: ")
+#     if user == "stop":
+#         break
+#     if user.isdigit():
+#         summa += int(user)
+#     else:
+#         print("Faqat son kiritsh mumkin !")
+#         continue
+# print(f"Summa = {summa}")

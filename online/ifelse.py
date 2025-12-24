beff7f60f2de3c1bb410607f8c9564faa3a1ec15
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-12-24 11:24:02 +0500

    update

---
+++ online/ifelse.py
@@ -0,0 +1,49 @@
+# x = 10
+# print(x > 12) # False
+
+# if , elif , else 
+
+# if - Agar 
+# elif aks holda yoki 
+# else aks holda 
+# user_number = int(input("Son kiriting : "))
+
+# if user_number == 18:
+#     print("OK !")
+# elif user_number == 17:
+#     print("1 yosh yetmayapti !")
+# elif user_number == 16:
+#     print("2 yosh yetmayapti !")
+# else:
+#     print("18 emas !")
+
+
+# user_number = int(input("Son kiriting : "))
+# num = 17
+# if user_number > 0:
+#     if user_number < 10:
+#         print("0 dan 10 dan kichik")
+#     if user_number > 10 and user_number < 20:
+#         print("10 dan katta 20 dan kichik ")
+#     if user_number == num:
+#         print("Topdiz!")
+# else:
+#     print("0 dan katta son kiriting !")
+
+# temp = 5
+# weather = "issiq" if temp > 10 else "sovuq"
+# print(weather)
+
+# if x > 5:print("Katta")
+
+
+    
+# task 1 
+# user son kiritadi , dastur son o'yledi , agar user kiritgan son dasturdagi songa teng bo'lsa "Yutdiz" ekranga chiqadi aks holsa user davom etadi 
+
+user = int(input("Son ? "))
+number = 7
+
+
+
+

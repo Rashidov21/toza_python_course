de208fbca5450e2c19ea8fddf549a7cc051b67b2
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2026-01-12 18:18:37 +0500

    update

--- online/homeworks.py
+++ online/homeworks.py
@@ -1,75 +1,23 @@
-# WHILE 1
-# a = 25
-# b = 7
-# while a >= b:
-#     a -= b
-# print(a)
 
-# WHILE 2
-# a = 24
-# b = 8
-# maksimum = 0
-# while a >= b:
-#     a -= b
-#     maksimum += 1
-# print(maksimum)
+# task 4 
+# userdan matn qabul qiling agar matnda yoki so'zda "o" harfi qatnashgan bo'lsa "a" harfiga almashtiring
+# input : apple 
+# output : opple
 
-# WHILE 3
-# n = 12
-# k = 5
-# maksimum = 0
-# while n >= k:
-#     n -= k
-#     maksimum += 1
-# print( maksimum)
-# print(n)
 
-# WHILE 4
-# n = 9
-# while n == 9:
-#     print(n," soni 3ning darajasi")
-# else: 
-#     print(n,"Soni 3 ning darajasi emas")
+# task 5
+# userdan so'z qabul qiling agar sozda son ishtirok etgan bolsa ekranga "True" chiqadi
+# input : salom12
+# output : True
+# input : salom
+# output : FAlse
 
-# WHILE 5
-# n = 16
-# temp = n
-# if n % 2 == 0:
-#     while temp < n:
-#         temp = (n - 2)
-#         print(n * (n - 2))
-#         n = n - 2
-# else:
-#     while temp < n:
-#         temp = (n - 1)
-#         print(n * (n - 1))
-#         n = n - 1
-# print(temp)
-    # task 3 
-# n soni berilgan (30 > n > 0) 0 dan n gacha bo'lgan sonlarni orasida probellar bilan chiqaring agar son toq bo'lsa bitta probel bilan uni keyingi son orasini belgilaysiz agar juft bo'lsa 2 ta probel bilan. misol : 0  1 2  3 4  5
 
-# 10 ta 2 xonalik butun son berilgan ulardan faqat 0 bilan tugaganlarini summasini hisoblang
+# Userdan 5 marta sonlar qabul qilin faqat butun sonlarni yigindisini hisoblang
 
-# 1 usul 
-import random 
-arr = []
-for i in range(10):
-    arr.append(random.randint(10,100))
-print(arr)
-summa = 0
-for k in arr:
-    if k % 10 == 0:
-        summa += k
-print("Summa : ", summa)
 
-# 2 - usul 
-arr2 = random.sample(range(10,100),10)
-print("Summa : ", arr2)
-print(sum([k for k in arr2 if k % 10 == 0]))
 
-# task 
-# user hohlagan miqdorda harf kiritadi 
-# agar "stop" deb yozsa dastur to'xtaydi 
-# va kiritgan harflarini ichida nechta unli harf bor ekanini ekranga chiqaradi 
-# input: a ,y ,h, t ,i 
-# output: 2 ta unli harf 
+# Userdan matnlarni qabul qiling ichida "python" so'zi ishtirok etgan bo'lsa "Tabriklaymiz topdingiz!" degan matn chiqsin
+
+
+# Userdan 10 ta string qabul qiling string da qanday belgilar bor ekanini hisoblang (harfmi, sonmi, butun son , qoldiq son) oxirida qaysindan nechta kiritilganini hisoblab chiqarinfg
---
+++ online/strings.py
@@ -0,0 +1,132 @@
+# Python - string 
+# s = 'salom'
+# print(type(s)) # <class 'str'>
+
+# \n - new line 
+# \t - tab 
+# \a - sound
+# s = "Python \n \t\tprogramming \a language"
+# print(s)
+
+# word = "don't \"worry\""
+# word = 'don't'
+# print(word)
+
+# index - joylashuv 
+# word = "python"
+# print(word[0])
+# print(word[1])
+# print(word[-1])
+# print(word[56]) # IndexError: string index out of range
+# word = "python"
+# print(word[-1])
+# print(word[-2])
+
+# word = "python"
+# new_word = word[4:]
+# print(new_word) # on
+ 
+# print(word[2:4]) # th
+
+# print(word[::-1]) # nohtyp
+
+# age = 30
+# salary = 1500
+# name = "Ali"
+# print(f"Your age : {age}, your salary : {salary}")
+# # Your age : 30, your salary : 1500
+
+# word = "Your name {0} Your salary {1}".format(name,salary)
+# print(word)
+# # Your name Ali
+
+# print("Your age %s" %age) #Your age 30
+
+# print(f""" Your name {name}
+#       Your age {age}
+#       Your salary {salary} """)
+
+# word = "python" 
+# print(len(word)) # 6
+# print(len(word) + 6) # 12
+
+# strip - qatordan ortiqcha probellarni ochirish uchun 
+# word = " python " 
+# print(len(word)) # 8
+# print(len(word.strip())) # 6
+
+# lstrip - qatordan ortiqcha probellarni ochirish uchun (chap tomondan)
+# print(len(word.lstrip())) # 7
+# rstrip - qatordan ortiqcha probellarni ochirish uchun (o'ng tomondan)
+# print(len(word.rstrip())) # 7
+
+# # split - qatorni siz korsatgan belgilari bo'yicha bo'lib ro'yhat qiladi 
+# word = "pytho'n i's bet'ter" 
+# print(word.split("'")) # ['pytho', 'n i', 's bet', 'ter']
+
+# print(word.rsplit(" ")) # ["pytho'n", "i's", "bet'ter"]
+
+# # splitlines - ko'p qatorli matnlarni \n bo'yicha bo'lib royhat qiladi \
+# # join - royhat elementlarini string qiladi 
+# arr = ["salom", "qale", "123"]
+# print("".join(arr)) # salomqale123
+# print(" ".join(arr)) # salom qale 123
+# print("-".join(arr)) # salom-qale-123
+
+
+# s = "Python is programming language"
+# print(s.upper())
+# print(s.lower())
+# print(s.title())
+# print(s.capitalize())
+
+    # print(chr(65)) # A
+    # print(ord("A")) # 65
+    
+# find - berilgan belgilarni qator ichidan qidiradi va indexini qaytaradi 
+# print("salom".find("o")) # 3
+# print("salom".find("t")) # -1
+# rfind - o'ngdan chapga qarab 
+
+# index - berilgan belgilarni qator ichidan qidiradi va indexini qaytaradi
+# rindex -  o'ngdan chapga qarab 
+
+# print("salom".index("o")) # 3
+# print("salom".index("t")) # ValueError
+
+# count - siz korsatgan belgini qator ichidan sanab nechtaligini qaytaradi (int)
+# print("banana".count("a")) # 3
+# print("banana".count("b")) # 1
+
+# startswith - agar matn siz korsatgan belgilar bilan boshlansa > True
+# print("salom".startswith('sa')) # True
+# print("salom".startswith('lo')) # False
+# endswith - agar matn siz korsatgan belgilar bilan tugasa > True
+# print("salom".endswith('sa')) # False
+# print("salom".endswith('lom')) # True
+
+# replace  - siz korsatgan belgilarni siz korsatgan belgilarga almashtirish  
+
+# print("salom".replace("m","n")) # salon
+
+
+print("abc123".isalnum()) # true
+print("abc123#".isalnum()) # false
+print('abc'.isalpha()) # True
+print('abc123'.isalpha()) # False
+print("12".isdigit()) # True
+print("12.5".isdigit()) # False
+
+print("12.5".isdecimal()) # False
+print("12".isdecimal()) # True
+
+print("abc123".isnumeric()) # False
+print("123".isnumeric()) # True
+
+print("salom".islower()) # True
+print("Salom".islower()) # False
+print("SALOM".isupper()) # True
+print("Salom".istitle()) # True
+
+print(" ".isspace()) # True
+print("#".isspace()) # False
